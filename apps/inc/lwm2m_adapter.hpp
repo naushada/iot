9a6f75cd1f7ce7aa01e99cca4974d6d782d66976
naushada <naushad.dln@gmail.com>
2024-03-07 00:46:46 +0530

    Updated

--- apps/inc/lwm2m_adapter.hpp
+++ apps/inc/lwm2m_adapter.hpp
@@ -1,9 +1,61 @@
 #ifndef __lwm2m_adapter_hpp__
 #define __lwm2m_adapter_hpp__
 
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <fstream>
 
 
+enum ObjectId_t : std::uint32_t {
+    SecurityObjectID = 0,
+    ServerObjectID = 1,
+    AccessControlObjectID = 2,
+    DeviceObjectID = 3,
+    ConnectivityMonitoringObjectID = 4,
+    FirmwareUpdateObjectID = 5,
+    LocationObjectID = 6,
+    ConnectivityStatisticsObjectID = 7,
 
+};
 
+enum TypeFieldOfTLV_t : std::uint8_t {
+  TypeBits76_ObjectInstance_OneOrMoreResourceTLV_00 = 0,
+  TypeBits76_ResourceInstance_OneOrMultipleResourceTLV_01 = 1,
+  TypeBits76_MultipleResource_OneOrMoreResourceInstanceTLV_10 = 2,
+  TypeBits76_ResourceWithValue_11 = 3
+};
+
+enum LengthFieldOfIdentifier_t : std::uint8_t {
+    TypeBit5_LengthOfTheIdentifier8BitsLong_0 = 0,
+    TypeBit5_LengthOfTheIdentifier16BitsLong_1 = 1,
+};
+
+struct TLV {
+    std::uint8_t m_type;
+    std::vector<std::uint8_t> m_identifier;
+    std::vector<std::uint8_t> m_length;
+    std::vector<std::uint8_t> m_value;
+    TLV() : m_type(0), m_identifier(2),m_length(3), m_value(1024) {}
+    ~TLV() = default;
+};
+
+class LwM2MAdapter {
+    public:
+        LwM2MAdapter();
+        ~LwM2MAdapter();
+
+        std::vector<TLV>& tlvs() {
+            return(m_tlvs);
+        }
+
+        std::int32_t parseLwM2MPayload(const std::string& uri, const std::string& payload, std::vector<TLV>& tlvs);
+        std::int32_t buildLwM2MPayload(const std::string& oid, const std::string& oiid, const std::string& orid, TLV& tlv);
+
+    private:
+        std::vector<TLV> m_tlvs;
+};
 
 #endif /*__lwm2m_adapter_hpp__*/
--- apps/src/lwm2m_adapter.cpp
+++ apps/src/lwm2m_adapter.cpp
@@ -1,9 +1,42 @@
 #ifndef __lwm2m_adapter_cpp__
 #define __lwm2m_adapter_cpp__
 
+extern "C" {
+     #include <libgen.h>
+}
+
+#include "lwm2m_adapter.hpp"
+
+LwM2MAdapter::LwM2MAdapter() {
+
+}
+
+LwM2MAdapter::~LwM2MAdapter() {
+
+}
+
+std::int32_t LwM2MAdapter::parseLwM2MPayload(const std::string& uri, const std::string& payload, std::vector<TLV>& tlvs) {
+    std::istringstream iss(payload);
+    iss.rdbuf()->pubsetbuf(const_cast<char *>(payload.data()), payload.length());
+    std::uint8_t onebyte;
+    do {
+        if(!iss.read(reinterpret_cast<char *>(&onebyte), sizeof(onebyte)).good()) {
+            std::cout <<basename(__FILE__) << ":" << __LINE__ << " input buffer is too small to process" << std::endl;
+            break;    
+        }
+
+        std::uint8_t typeValueOf76Bits = (onebyte & 0b11000000) >> 6;
+        std::uint8_t typeValueOf5thBit = (onebyte & 0b00100000) >> 5;
+        std::uint8_t typeValueOf43Bits = (onebyte & 0b00011000) >> 3;
+        std::uint8_t typeValueOf20Bits = (onebyte & 0b00000111) >> 0;
 
 
+    } while(0);
+}
 
+std::int32_t LwM2MAdapter::buildLwM2MPayload(const std::string& oid, const std::string& oiid, const std::string& orid, TLV& tlv) {
+
+}
 
 
 
