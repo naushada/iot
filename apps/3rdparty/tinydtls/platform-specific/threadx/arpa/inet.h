3d3afb43a362cbcfb66c7c988e405ac65532cbfa
naushada <naushad.dln@gmail.com>
2024-03-14 06:58:12 +0530

    Initial Commit

---
+++ apps/3rdparty/tinydtls/platform-specific/threadx/arpa/inet.h
@@ -0,0 +1,15 @@
+/**
+ * @file inet.h
+ *
+ * Header file for definitions for internet operations
+ *
+ * Copyright (C) Sierra Wireless Inc.
+ *
+ */
+
+#ifndef _LWM2MCORE_ARPA_INET_H
+#define _LWM2MCORE_ARPA_INET_H
+
+#include "legato.h"
+
+#endif /* _LWM2MCORE_ARPA_INET_H */
---
+++ apps/3rdparty/tinydtls/platform/types.h
@@ -0,0 +1,26 @@
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*__TYPES_H__*/
